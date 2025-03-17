0ab5569c1933c12dad0b7964780b49a6a86b66c1
sodium7063 <zht7063@163.com>
2025-03-17 09:15:08 +0800

    pyside -> changed ide form trae to vsc

--- pyside6/04.real_ligin.py
+++ pyside6/04.real_ligin.py
@@ -1,14 +1,24 @@
 from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtCore import Qt
-from login import Ui_MainWindow
+from login import Ui_Login_UI
 
 
 class Main_Window(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.ui = Ui_MainWindow()  # 创建Ui_MainWindow类的实例
+        self.ui = Ui_Login_UI()  # 创建Ui_MainWindow类的实例
         self.ui.setupUi(self)  # 调用setupUi方法，将UI布局添加到主窗口中
+        self.ui.pushButton.clicked.connect(self.loginFunc)
+
+    def loginFunc(self):
+        username = self.ui.userNameLineEdit.text()
+        password = self.ui.passwordLineEdit.text()
+        print(f"用户名：{username}，密码：{password}")
+        if username == "admin" and password == "123456":
+            print("登录成功")
+        else:
+            print("登录失败")
 
 
 def main():
--- pyside6/login.py
+++ pyside6/login.py
@@ -18,12 +18,12 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
 from PySide6.QtWidgets import (QApplication, QLabel, QLineEdit, QMainWindow,
     QPushButton, QSizePolicy, QStatusBar, QWidget)
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        if not MainWindow.objectName():
-            MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(279, 204)
-        self.centralwidget = QWidget(MainWindow)
+class Ui_Login_UI(object):
+    def setupUi(self, Login_UI):
+        if not Login_UI.objectName():
+            Login_UI.setObjectName(u"Login_UI")
+        Login_UI.resize(279, 204)
+        self.centralwidget = QWidget(Login_UI)
         self.centralwidget.setObjectName(u"centralwidget")
         self.label = QLabel(self.centralwidget)
         self.label.setObjectName(u"label")
@@ -33,32 +33,32 @@ class Ui_MainWindow(object):
         self.label_2.setObjectName(u"label_2")
         self.label_2.setGeometry(QRect(30, 80, 80, 30))
         self.label_2.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.lineEdit = QLineEdit(self.centralwidget)
-        self.lineEdit.setObjectName(u"lineEdit")
-        self.lineEdit.setGeometry(QRect(120, 30, 113, 21))
-        self.lineEdit_2 = QLineEdit(self.centralwidget)
-        self.lineEdit_2.setObjectName(u"lineEdit_2")
-        self.lineEdit_2.setGeometry(QRect(120, 80, 113, 21))
+        self.userNameLineEdit = QLineEdit(self.centralwidget)
+        self.userNameLineEdit.setObjectName(u"userNameLineEdit")
+        self.userNameLineEdit.setGeometry(QRect(120, 30, 113, 21))
+        self.passwordLineEdit = QLineEdit(self.centralwidget)
+        self.passwordLineEdit.setObjectName(u"passwordLineEdit")
+        self.passwordLineEdit.setGeometry(QRect(120, 80, 113, 21))
         self.pushButton = QPushButton(self.centralwidget)
         self.pushButton.setObjectName(u"pushButton")
         self.pushButton.setGeometry(QRect(100, 140, 75, 23))
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.statusbar = QStatusBar(MainWindow)
+        Login_UI.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(Login_UI)
         self.statusbar.setObjectName(u"statusbar")
-        MainWindow.setStatusBar(self.statusbar)
+        Login_UI.setStatusBar(self.statusbar)
 
-        self.retranslateUi(MainWindow)
+        self.retranslateUi(Login_UI)
 
-        QMetaObject.connectSlotsByName(MainWindow)
+        QMetaObject.connectSlotsByName(Login_UI)
     # setupUi
 
-    def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"\u767b\u5f55\u754c\u9762", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"\u7528\u6237\u540d\uff1a", None))
-        self.label_2.setText(QCoreApplication.translate("MainWindow", u"\u5bc6\u7801\uff1a", None))
-        self.lineEdit.setText("")
-        self.lineEdit.setPlaceholderText(QCoreApplication.translate("MainWindow", u"\u8bf7\u8f93\u5165\u7528\u6237\u540d", None))
-        self.lineEdit_2.setPlaceholderText(QCoreApplication.translate("MainWindow", u"\u8bf7\u8f93\u5165\u5bc6\u7801", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"\u767b\u5f55", None))
+    def retranslateUi(self, Login_UI):
+        Login_UI.setWindowTitle(QCoreApplication.translate("Login_UI", u"\u767b\u5f55\u754c\u9762", None))
+        self.label.setText(QCoreApplication.translate("Login_UI", u"\u7528\u6237\u540d\uff1a", None))
+        self.label_2.setText(QCoreApplication.translate("Login_UI", u"\u5bc6\u7801\uff1a", None))
+        self.userNameLineEdit.setText("")
+        self.userNameLineEdit.setPlaceholderText(QCoreApplication.translate("Login_UI", u"\u8bf7\u8f93\u5165\u7528\u6237\u540d", None))
+        self.passwordLineEdit.setPlaceholderText(QCoreApplication.translate("Login_UI", u"\u8bf7\u8f93\u5165\u5bc6\u7801", None))
+        self.pushButton.setText(QCoreApplication.translate("Login_UI", u"\u767b\u5f55", None))
     # retranslateUi
 
